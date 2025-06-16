a8e0d3bd028deb3545c3cd528e7f443e4676bf82
Onesiphore14 <dossouonesiphore@gmail.com>
2025-06-16 12:37:13 +0100

    Update inscription.js

--- inscription.js
+++ inscription.js
@@ -1,1 +1,26 @@
 
+document.getElementById('registerForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const nom =
+        document.getElementById('nom').value.trim();
+    const email =
+        document.getElementById('email').value.trim();
+
+    const numero =
+        document.getElementById('numero').value.trim();
+    const password =
+        document.getElementById('password').value;
+    const confirmer =
+        document.getElementById('confirmer').value;
+    if (nom === '' || email === '' || numero === '' || password === '' || confirmer === '') {
+        alert("Tous les champs sont obligatoires.");
+        return;
+    }
+    if (password !== confirmer) {
+        alert("Les moys de passe ne correspondent pas");
+        return;
+        window.location.href = "accueil.html"
+    }
+});
+
